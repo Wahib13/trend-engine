2d4009ce402c404c47cfe7015dd264cf97b0fdc7
Wahib Farhat <wahibfarhat13@gmail.com>
2026-01-26 13:48:56 +0000

    setup alembic and installed psycopg2-binary for database connection

--- src/db/initialise.py
+++ src/db/initialise.py
@@ -2,8 +2,6 @@ from db.connection import Base
 from db.models import Source, SourceName, Feed, FeedType
 
 
-def initialise_database(engine, session, default_data):
-    Base.metadata.create_all(engine)
-
+def initialise_database(session, default_data):
     session.add_all(default_data)
     session.commit()
